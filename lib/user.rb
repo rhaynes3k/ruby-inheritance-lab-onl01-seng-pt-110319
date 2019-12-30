bc633830634a6993cd91fb90780440dc3ba8688d
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-30 23:30:07 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,15 @@
 class User
-  attr_accessor :first_name, :last_name
+  #attr_accessor :first_name, :last_name
   
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(#first_name, last_name)
+    # @first_name = first_name
+    # @last_name = last_name
   end
   
-
+  def first_name= (first_name)
+    @first_name = first_name
+  end
 
 
 
